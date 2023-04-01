d18ad528ec0ec7bb12f13a9a4b3f192d411e4460
xWaszka <jestemwaszka@gmail.com>
2023-04-01 10:41:19 +0200

    2023-04-01 MW

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # CeneoScraper
+## Selektory CSS
+| składowa | nazwa | selektor |
+| --- | --- | --- |
+| opinia | opinion | div.js\_product-review |
+| identyfikator opinii | opinion\_id | ["data-entry-id"] |
+| autor | author | span.user-post\_\_author-name |
+| rekomendacja | recommendation | span.user-post\_\_author-recomendation \> em |
+| liczba gwiazdek | score | span.user-post\_\_score-count |
+| czy opinia jest potwierdzona zakupem | confirmed | div.review-pz |
+| data wystawienia opinii | opinion\_date | span.user-post\_\_published \> time:nth-child(1)["datetime"] |
+| data zakupu produktu | purchase\_date | span.user-post\_\_published \> time:nth-child(2)["datetime"] |
+| ile osób uznało opinię za przydatną | up\_votes | span[id^="votes-yes"] |
+| ile osób uznało opinię za nieprzydatną | down\_votes | span[id^="votes-no"] |
+| treść opinii | content | div.user-post\_\_text |
+| listę wad | cons | div.review-feature\_\_col:has(\> div.review-feature\_\_title--negatives) \> div.review-feature\_\_item |
+| listę zalet | pros | div.review-feature\_\_col:has(\> div.review-feature\_\_title--positives) \> div.review-feature\_\_item |
--- scraper.py
+++ scraper.py
@@ -1,6 +1,33 @@
+import json
 import requests
+from bs4 import BeautifulSoup
+
+def get_element():
+    pass
+
 # produkt_code = input("Podaj kod produktu: ")
 product_code = "100361771"
 url = f"https://www.ceneo.pl/{product_code}#tab=reviews"
 response = requests.get(url)
-print(response.status_code)
+# print(response.status_code)
+
+if response.status_code == requests.codes.ok:
+    page_dom = BeautifulSoup(response.text, "html.parser")
+    opinions = page_dom.select("div.js_product-review")
+    opinions_all = []
+    for opinion in opinions:
+        single_opinion = {
+            "opinion_id": opinion["data-entry-id"],
+            "author": opinion.select_one("span.user-post__author-name").get_text().strip(),
+            "recommendation": opinion.select_one("span.user-post__author-recomendation").get_text().strip(),
+            "score": opinion.select_one("span.user-post__score-count").get_text().strip(),
+            "confirmed": opinion.select_one("div.review-pz").get_text().strip(),
+            "opinion_date": opinion.select_one("span.user-post__published > time:nth-child(1)")["datetime"].strip(),
+            "purchase_date": opinion.select_one("span.user-post__published > time:nth-child(2)")["datetime"].strip(),
+            "up_votes": opinion.select_one("span[id^='votes-yes']").get_text().strip(),
+            "down_votes": opinion.select_one("span[id^='votes-no']").get_text().strip(),
+            "content": opinion.select_one("div.user-post__text").get_text().strip(),
+            "cons": [p.get_text().strip() for p in opinion.select("div.review-feature__col:has(> div.review-feature__title--negatives) > div.review-feature__item")],
+            "pros": [p.get_text().strip() for p in opinion.select("div.review-feature__col:has(> div.review-feature__title--positives) > div.review-feature__item")],
+        }
+print(json.dumps(single_opinion, indent=4,ensure_ascii=False))
